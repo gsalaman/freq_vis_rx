db8ca255a7f8d22ce0f5b5995eb8879457ee67a6
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-02-19 17:59:40 -0700

    color pallete and 115200 xbee
    May be too fast...

--- freq_vis_rx.ino
+++ freq_vis_rx.ino
@@ -37,12 +37,49 @@ int freq[FREQ_BINS];
 
 #define START_CHAR 's'
 
+// Color pallete for spectrum...cooler than just single green.
+uint16_t spectrum_colors[] = 
+{
+  matrix.Color444(15,0,0),   // index 0
+  matrix.Color444(13,1,0),   // index 1
+  matrix.Color444(12,3,0),   // index 2
+  matrix.Color444(10,4,0),   // index 3
+  matrix.Color444(9,6,0),   // index 4
+  matrix.Color444(7,7,0),   // index 5
+  matrix.Color444(6,9,0),   // index 6
+  matrix.Color444(4,10,0),   // index 7 
+  matrix.Color444(3,12,0),   // index 8
+  matrix.Color444(1,13,0),   // index 9
+  matrix.Color444(0,15,0),   // index 10
+  matrix.Color444(0,13,1),   // index 11
+  matrix.Color444(0,12,3),   // index 12 
+  matrix.Color444(0,10,4),   // index 13
+  matrix.Color444(0,9,6),   // index 14
+  matrix.Color444(0,7,7),   // index 15
+  matrix.Color444(0,6,9),   // index 16
+  matrix.Color444(0,4,10),   // index 17
+  matrix.Color444(0,3,12),   // index 18
+  matrix.Color444(0,1,13),   // index 19
+  matrix.Color444(0,0,15),   // index 20
+  matrix.Color444(1,0,13),   // index 21
+  matrix.Color444(3,0,12),   // index 22
+  matrix.Color444(4,0,10),   // index 23
+  matrix.Color444(6,0,9),   // index 24
+  matrix.Color444(7,0,7),   // index 25
+  matrix.Color444(9,0,6),   // index 26
+  matrix.Color444(10,0,4),   // index 27
+  matrix.Color444(11,0,3),   // index 28
+  matrix.Color444(12,0,2),   // index 29
+  matrix.Color444(13,0,1),   // index 30
+  matrix.Color444(15,0,0),   // index 31  
+};
+
 void setup() 
 {
   matrix.begin();
   
   Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial1.begin(115200);
 
   Serial.println("Freq RX initialized");
 }
@@ -86,7 +123,7 @@ void display_freq( void )
      
     x = i*2;
     
-    matrix.drawRect(x,32,2,0-mag, matrix.Color333(1,0,0));
+    matrix.drawRect(x,32,2,0-mag, spectrum_colors[i]);
   }
 
   matrix.swapBuffers(true);
